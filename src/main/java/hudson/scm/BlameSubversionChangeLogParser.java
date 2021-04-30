86ba80813a4f7620b773ac6a7fd40ba5c5160c11
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2021-04-30 17:20:31 +0200

    Secure digester

--- src/main/java/hudson/scm/BlameSubversionChangeLogParser.java
+++ src/main/java/hudson/scm/BlameSubversionChangeLogParser.java
@@ -30,6 +30,7 @@ import hudson.util.IOException2;
 import org.apache.commons.digester3.Digester;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -42,10 +43,26 @@ import java.util.ArrayList;
  * @author tang,Kohsuke Kawaguchi
  */
 public class BlameSubversionChangeLogParser extends ChangeLogParser {
+    private Digester createDigester(boolean secure) throws SAXException {
+        final Digester digester = new Digester();
+        if (secure) {
+            digester.setXIncludeAware(false);
+            try {
+                digester.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                digester.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                digester.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                digester.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            } catch (ParserConfigurationException ex) {
+                throw new SAXException("Failed to securely configure xml digester parser", ex);
+            }
+        }
+        return digester;
+    }
+
     public BlameSubversionChangeLogSet parse(AbstractBuild build, File changelogFile) throws IOException, SAXException {
         // http://svn.collab.net/repos/svn/trunk/subversion/svn/schema/
 
-        Digester digester = new Digester();
+        Digester digester = createDigester(!Boolean.getBoolean(this.getClass().getName() + ".UNSAFE"));
         ArrayList<LogEntry> r = new ArrayList<LogEntry>();
         digester.push(r);
 
